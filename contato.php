df4f7b45d8fb089d60a37c7c5ef1f809bf7e3660
lucascarlos1661 <lucascarlos1661@gmail.com>
2021-06-22 18:42:42 -0300

    Alteração do arquivo contato

--- contato.php
+++ contato.php
@@ -18,7 +18,7 @@ include("header.php")
     <main role="main" class="container">
         <h1 class="mt-5">Contato</h1>
         <br>
-        <h3>Entre em contato com a gente através do e-mail ou WhatsApp:</h3>
+        <h3>Entre em contato com a gente através do e-mail ou WhatsApp!</h3>
         <p><strong>Bruno Madeira:</strong> +55 31 99974-1540, bruno.madeira@a.unileste.edu.br</p>
         <p><strong>Dener Maradona:</strong> +55 31 98507-3118, dener.santos@a.unileste.edu.br</p>
         <p><strong>Lucas Carlos:</strong> +55 31 995838633, lucas.carlos@a.unileste.edu.br</p>
